a83d00a511c9a92ee17aa57c90e5a30d2857a400
nzkks <kkshanthosh@gmail.com>
2024-07-28 22:10:12 +1200

    Add Theme Switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { ThemeSwitch } from '@/components';
 import { Button } from '@/components/ui/button';
 
 const Home = () => {
   return (
     <main>
+      <ThemeSwitch />
       <p>Hello</p>
       <Button>Button</Button>
     </main>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as ThemeProvider } from './themeProvider';
 export * from './themeProvider';
+
+export { default as ThemeSwitch } from './themeSwitch';
+export * from './themeSwitch';
