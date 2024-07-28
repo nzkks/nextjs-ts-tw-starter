e0be83278f9dcdc3ae6167c0227b7478674c8cd9
nzkks <kkshanthosh@gmail.com>
2024-07-29 11:07:19 +1200

    Add css colour variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import { ThemeSwitch } from '@/components';
 const Home = () => {
   return (
     <main>
-      <div className="mb-5">
-        <p className="text-primary text-2xl">Hello</p>
+      <div className="mb-5 bg-primary">
+        <p className="text-2xl text-primary-foreground">Hello</p>
+        <button className="font-bold">Button</button>
+      </div>
+      <div className="mb-5 bg-secondary">
+        <p className="text-2xl text-secondary-foreground">Hello</p>
         <button className="font-bold">Button</button>
       </div>
       <ThemeSwitch />
