3abd877a77ff730f67f1301fad41388d95875f53
nzkks <kkshanthosh@gmail.com>
2024-07-29 10:21:06 +1200

    Add Theme switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import { ThemeSwitch } from '@/components';
+
 const Home = () => {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <div className="mb-5">
+        <p className="text-primary text-2xl">Hello</p>
+        <button className="font-bold">Button</button>
+      </div>
+      <ThemeSwitch />
+    </main>
+  );
 };
 
 export default Home;
---
+++ src/components/themeSwitch/theme-switch.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+
+const ThemeSwitch = () => {
+  const { theme, setTheme } = useTheme();
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  );
+};
+
+export default ThemeSwitch;
